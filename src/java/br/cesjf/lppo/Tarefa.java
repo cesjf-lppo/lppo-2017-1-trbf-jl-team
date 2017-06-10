ead1bed48cbf96ac6a0d77c0c78e1df332b18012
Luiz Santos <Luiz Santos@DESKTOP-IBSNJE5>
2017-06-09 21:22:24 -0300

    Criação da classe Tarefa

---
+++ src/java/br/cesjf/lppo/Tarefa.java
@@ -0,0 +1,53 @@
+package br.cesjf.lppo;
+
+import java.util.Date;
+
+public class Tarefa {
+    private Long id;
+    private String titulo, descricao;
+    private Date dataConcluir, dataConclusao;
+
+    public Tarefa() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataConcluir() {
+        return dataConcluir;
+    }
+
+    public void setDataConcluir(Date dataConcluir) {
+        this.dataConcluir = dataConcluir;
+    }
+
+    public Date getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(Date dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+    
+}
