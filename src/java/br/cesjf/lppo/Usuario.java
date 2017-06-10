f99b8ac6bf492e7854c3cbca207c7f9c9c9b8780
Luiz Santos <Luiz Santos@DESKTOP-IBSNJE5>
2017-06-09 21:30:37 -0300

    Criação do PU e do Entity (Usuario)

--- src/java/br/cesjf/lppo/Usuario.java
+++ src/java/br/cesjf/lppo/Usuario.java
@@ -1,14 +1,21 @@
 package br.cesjf.lppo;
 
-public class Usuario {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome, email, senha;
 
     public Usuario(){
 
     }
-    
-    
 
     public Long getId() {
         return id;
