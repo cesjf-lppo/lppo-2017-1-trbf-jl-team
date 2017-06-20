83ab118189c9f4c244fc1a8da8820fd26ff469e9
Jéssica Barbosa <je_barbosaa@hotmail.com>
2017-06-19 22:17:42 -0300

    Criação dos metodos na Classe UsuarioServlet

--- src/java/br/cesjf/lppo/servlets/UsuarioServlet.java
+++ src/java/br/cesjf/lppo/servlets/UsuarioServlet.java
@@ -1,7 +1,12 @@
 package br.cesjf.lppo.servlets;
 
+import br.cesjf.lppo.Usuario;
+import br.cesjf.lppo.dao.UsuarioJpaController;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
@@ -51,25 +56,77 @@ public class UsuarioServlet extends HttpServlet {
             throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/novo-usuario.jsp").forward(request, response);
     }
+    
+     private void doCriarPost(HttpServletRequest request, HttpServletResponse response)           
+             throws ServletException, IOException {
+        Usuario usuario1 = new Usuario();
+        usuario1.setNome(request.getParameter("nome"));
+        usuario1.setEmail(request.getParameter("email"));
+        usuario1.setSenha(request.getParameter("senha"));
 
-    private void doListarrGet(HttpServletRequest request, HttpServletResponse response)            
+        UsuarioJpaController dao = new UsuarioJpaController(ut, emf);
+        try {
+            dao.create(usuario1);
+            response.sendRedirect("listar-usuario.html");
+        } catch (Exception ex) {
+            Logger.getLogger(UsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+       
+    
+        private void doListarrGet(HttpServletRequest request, HttpServletResponse response)            
             throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         List<Usuario> usuarios;
+        
+        UsuarioJpaController dao = new UsuarioJpaController(ut, emf);
+        usuarios = dao.findUsuarioEntities();
+        
+        request.setAttribute("usuarios", usuarios);
+        request.getRequestDispatcher("WEB-INF/listar-usuario.jsp").forward(request, response);
     }
+    
+    private void doEditarGet(HttpServletRequest request, HttpServletResponse response)          
+            throws ServletException, IOException {
+         try {
+        UsuarioJpaController dao = new UsuarioJpaController(ut, emf);
+        
+        Long id = Long.parseLong(request.getParameter("id"));
+        Usuario usuario = dao.findUsuario(id);
+        request.setAttribute("usuario", usuario);
+        request.getRequestDispatcher("WEB-INF/editar-usuario.jsp").forward(request, response);
+    } catch (Exception e) {
+        response.sendRedirect("listar-usuario.html");
+    }
+}
 
-    private void doExcluirGet(HttpServletRequest request, HttpServletResponse response)            throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void doEditarPost(HttpServletRequest request, HttpServletResponse response)            
+            throws ServletException, IOException {
+           try {
+        UsuarioJpaController dao = new UsuarioJpaController(ut, emf);
+        
+        Long id = Long.parseLong(request.getParameter("id"));
+        Usuario usuario = dao.findUsuario(id);
+        usuario.setNome(request.getParameter("nome"));
+        usuario.setEmail(request.getParameter("email"));
+        usuario.setSenha(request.getParameter("senha"));
+        dao.edit(usuario);
+        response.sendRedirect("listar-usuario.html");
+       
+    } catch (Exception e) {
+        response.sendRedirect("listar-usuario.html");
     }
-
-    private void doEditarGet(HttpServletRequest request, HttpServletResponse response)            throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+ }
+     private void doExcluirGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        try {
+            UsuarioJpaController dao = new UsuarioJpaController(ut, emf);
+        
+            Long id = Long.parseLong(request.getParameter("id"));
+            dao.destroy(id);
+        } catch (Exception ex) {
+        }
+        response.sendRedirect("listar-usuario.html");
     }
-
-    private void doCriarPost(HttpServletRequest request, HttpServletResponse response)            throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void doEditarPost(HttpServletRequest request, HttpServletResponse response)            throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
 }
